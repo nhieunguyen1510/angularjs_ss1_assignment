aadd7fa5ee4ec85216fa72f63e6e7d2fab421faf
Nhieu Nguyen Van <nhieunguyenv@HarveyNash.vn.local>
2016-06-10 16:50:38 +0700

    update product list

--- server/services/productService.js
+++ server/services/productService.js
@@ -7,50 +7,50 @@ var ProductService = {};
 
 ProductService.getProducts = function getProducts(category) {
     var deferred = Q.defer();
-    
+
     if (category) {
-            dbConnection.query(
-                "SELECT p.id, p.name, p.thumbnail, p.price FROM product p INNER JOIN category c ON p.category_id = c.id WHERE c.name = ?", category, 
-                function (err, result) {
-                    if(err) {
-                        deferred.reject(err);
-                    } else {
-                        deferred.resolve(result);
-                    }
+        dbConnection.query(
+            "SELECT p.id, p.name, p.description, p.thumbnail, p.price FROM product p INNER JOIN category c ON p.category_id = c.id WHERE c.name = ?", category,
+            function(err, result) {
+                if (err) {
+                    deferred.reject(err);
+                } else {
+                    deferred.resolve(result);
                 }
-            );
-            
+            }
+        );
+
         return deferred.promise;
     }
-    
+
     dbConnection.query(
-        "SELECT id, name, thumbnail, price FROM product", 
-        function (err, result) {
-            if(err) {
-                 deferred.reject(err);
+        "SELECT id, name, description, thumbnail, price FROM product",
+        function(err, result) {
+            if (err) {
+                deferred.reject(err);
             } else {
                 deferred.resolve(result);
             }
         }
     );
-    
+
     return deferred.promise;
 };
 
 ProductService.getProduct = function getProduct(id) {
     var deferred = Q.defer();
-    
+
     dbConnection.query(
         "SELECT id, name, thumbnail, price, description FROM product where id = ?", id,
-        function (err, result) {
-            if(err) {
-                 deferred.reject(err);
+        function(err, result) {
+            if (err) {
+                deferred.reject(err);
             } else {
                 deferred.resolve(result);
             }
         }
     );
-    
+
     return deferred.promise;
 }
 
